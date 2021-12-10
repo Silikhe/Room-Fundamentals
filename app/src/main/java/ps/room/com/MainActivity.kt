6fb99d77d2a9fa3fc42b3e24cd8c32bcb57e0b47
Silikhe <silaswans@gmail.com>
2021-12-10 07:43:23 +0300

    refactor(init):refactor files[Finish #180593530]

--- app/src/main/java/ps/room/com/MainActivity.kt
+++ app/src/main/java/ps/room/com/MainActivity.kt
@@ -8,12 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i(MainActivity.TAG, "Owner ON_CREATE ")
+//        Log.i(MainActivity.TAG, "Owner ON_CREATE ")
 
-        lifecycle.addObserver(MainActivityObserver())
+//        lifecycle.addObserver(MainActivityObserver())
     }
 
-    companion object{
-        private val TAG : String = MainActivity::class.java.simpleName
-    }
+//    companion object{
+//        private val TAG : String = MainActivity::class.java.simpleName
+//    }
 }
